26bf25ecea620a00f78ac502ccb327d6ee812cb8
Juan Esteban Nieto Cifuentes <89233604+Jenietoc@users.noreply.github.com>
2023-02-15 08:46:19 -0500

    #5 Create module for members (#9)
    * Create module for members
    
    * Change cargo.toml
    
    * Fix error with version
    
    * New changes required
    
    * Create a const in module member
    
    * new changes in comments and members
    
    * New format for modules

---
+++ mtk-automation/src/contract_actions/datakey.rs
@@ -0,0 +1,15 @@
+//! Module DataKey
+//! 
+//! Module for defining the types of keys within the contract
+use soroban_sdk::contracttype;
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    OrgName,
+    TokenId,
+    AdminId,
+    Rewards,
+    Members,
+    AllowedF
+}
