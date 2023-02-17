f9203b586537ad72c98c008db484665b79f32df4
Juan Esteban Nieto Cifuentes <89233604+Jenietoc@users.noreply.github.com>
2023-02-17 11:23:39 -0500

    #7 Create a module for rewards (#10)
    * Create a module reward
    
    * Change name reward for compensate
    
    * Create a new module token_operation
    
    * restructring some code
    
    * change name module for reward

--- mtk-automation/src/contract_actions/mod.rs
+++ mtk-automation/src/contract_actions/mod.rs
@@ -4,6 +4,8 @@ pub mod member;
 pub mod token_contract;
 pub mod admin;
 pub mod identifier_wrapper;
+pub mod reward;
+pub mod token_operation;
 pub mod token {
   soroban_sdk::contractimport!(file = "./soroban_token_spec.wasm");
 }
