9d240fdfb49fbeefdcb9b88933699a2174cc899b
Juan Esteban Nieto Cifuentes <89233604+Jenietoc@users.noreply.github.com>
2023-02-17 11:23:39 -0500

    #7 Create a module for rewards (#10)
    * Create a module reward
    
    * Change name reward for compensate
    
    * Create a new module token_operation
    
    * restructring some code
    
    * change name module for reward

--- mtk-automation/src/lib.rs
+++ mtk-automation/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
-use soroban_sdk::{contractimpl, Env, Vec, BytesN, AccountId};
+use soroban_sdk::{contractimpl, Env, Symbol, Vec, BytesN, AccountId, Map};
 
-use soroban_auth::{Identifier};
+use soroban_auth::{Identifier, Signature};
 
 mod contract_actions;
 
@@ -12,6 +12,7 @@ pub trait OrganizationContractTrait {
     fn initialize(
         e: Env,
         admin: Identifier,
+        compensations: Map<Symbol, u32>,
         token_c_id:BytesN<32>
     );
 
@@ -21,11 +22,14 @@ pub trait OrganizationContractTrait {
     /// revoke to the organization
     fn revoke_m(env: Env, from: AccountId);
 
+    /// compensate a member to the organization
+    fn comp_m(e: Env, token_approval_sig: Signature, to: AccountId, r_type: Symbol);
+
     /// get token contract to the organization
     fn get_tc_id(env: Env) -> BytesN<32>;
     
     /// get members to the organization
-    fn get_m(env: Env) -> Vec<AccountId>; 
+    fn get_m(env: Env) -> Vec<AccountId>;
 }
 
 #[contractimpl]
@@ -33,11 +37,14 @@ impl OrganizationContractTrait for OrganizationContract {
     fn initialize(
         env: Env, 
         admin: Identifier,
+        compensations: Map<Symbol, u32>,
         token_c_id: BytesN<32>
     ) {
         contract_actions::admin::set_admin_id(&env, &admin);
 
         contract_actions::token_contract::set_token_id(&env, &token_c_id);
+
+        contract_actions::reward::set_rewards(&env, &compensations);
     }
 
     fn add_m(env: Env, account: AccountId) {
@@ -46,6 +53,10 @@ impl OrganizationContractTrait for OrganizationContract {
     
     fn revoke_m(env: Env, from: AccountId) {
         contract_actions::member::revoke_membership(&env, &from);
+    }
+
+    fn comp_m(env: Env, approval_sign: Signature, to: AccountId, r_type: Symbol) {
+        contract_actions::reward::reward_member(&env, &approval_sign, &to, &r_type);
     }
     
     fn get_tc_id(env: Env) -> BytesN<32> {
