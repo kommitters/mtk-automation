3262c90e2b3a43bb0651dfc4a560d1d755cbc410
David Roldán <62166813+Odraxs@users.noreply.github.com>
2023-02-17 14:52:39 -0500

    Add CI/CD actions (#13)
    * Add CI/CD
    
    * Remove code warnings
    
    * Add cd execution with version branches 
    
    Fix warnings from rebase
    
    * Remove staging workflow

--- mtk-automation/src/contract_actions/reward.rs
+++ mtk-automation/src/contract_actions/reward.rs
@@ -13,23 +13,23 @@ pub fn set_rewards(env: &Env, compensate_types: &Map<Symbol, u32>) {
 }
 
 pub fn reward_member(env: &Env, approval_sign: &Signature, to: &AccountId, compensate_types: &Symbol) {
-    if !member::is_member(&env, &to) {
+    if !member::is_member(env, to) {
         panic!("The user account you're trying to compensate doesn't belong to the organization");
     }
 
-    if !is_reward_valid(&env, &compensate_types) {
+    if !is_reward_valid(env, compensate_types) {
         panic!("The compensate type you are trying to use isn't supported")
     }
 
-    let compensate_value = get_compensation_by_type(&env, &compensate_types);
-    token_operation::transfer(&env, &approval_sign, &identifier_wrapper::get_account_identifier(to.clone()), &compensate_value);
+    let compensate_value = get_compensation_by_type(env, compensate_types);
+    token_operation::transfer(env, approval_sign, &identifier_wrapper::get_account_identifier(to.clone()), &compensate_value);
 }
 
 fn get_compensation_by_type(env: &Env, r_type: &Symbol) -> i128 {
     let key = DataKey::Rewards;
     let compensate: Map<Symbol, i128> = env.storage().get(key).unwrap().unwrap();
 
-    compensate.get(r_type.clone()).unwrap().unwrap()
+    compensate.get(*r_type).unwrap().unwrap()
 }
 
 fn get_rewards(env: &Env) -> Map<Symbol, u32> {
@@ -38,6 +38,6 @@ fn get_rewards(env: &Env) -> Map<Symbol, u32> {
 }
 
 fn is_reward_valid(env: &Env, key: &Symbol) -> bool {
-    let compensates = get_rewards(&env);
-    compensates.contains_key(key.clone())
+    let compensates = get_rewards(env);
+    compensates.contains_key(*key)
 }
