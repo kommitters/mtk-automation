a049a4439a7108332155ff568554fa5075ad65ad
David Roldán <62166813+Odraxs@users.noreply.github.com>
2023-02-20 18:19:23 -0500

    Update sdk version (#19)
    * Update to v0.6
    
    Also run `cargo fmt` command
    
    * Add requested changes
    
    * Add description to Enum key

--- mtk-automation/src/contract_actions/fund.rs
+++ mtk-automation/src/contract_actions/fund.rs
@@ -1,23 +1,23 @@
 //! Module for Fund
-//! 
+//!
 //! Module for managing the organization's funds
-use soroban_sdk::Env;
-use crate::contract_actions::datakey::DataKey;
 use crate::contract_actions::admin;
+use crate::contract_actions::datakey::DataKey;
 use crate::contract_actions::token;
 use crate::contract_actions::token_contract;
+use soroban_sdk::Env;
 
-pub fn set_available_funds_to_issue(env: &Env, new_value: i128){
-    env.storage().set(DataKey::AllowedF, new_value);
+pub fn set_available_funds_to_issue(env: &Env, new_value: i128) {
+    env.storage().set(&DataKey::AllowedF, &new_value);
 }
 
 pub fn get_available_funds_to_issue(env: &Env) -> i128 {
-    env.storage().get(DataKey::AllowedF).unwrap().unwrap()
+    env.storage().get(&DataKey::AllowedF).unwrap().unwrap()
 }
 
 pub fn get_contract_balance(env: &Env) -> i128 {
     let tc_id = token_contract::get_token_contract_id(env);
-    let client = token::Client::new(env, tc_id);
+    let client = token::Client::new(env, &tc_id);
 
     let admin_id = admin::get_admin_id(env);
 
