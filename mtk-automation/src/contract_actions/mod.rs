58d0a6a64c17c3e54edb1645135cb4eee0b7e4cc
David Roldán <62166813+Odraxs@users.noreply.github.com>
2023-02-23 11:43:06 -0500

    Add test to contract (#20)
    * Add test and type change 
    
    * Fix warnings
    
    * Add missing function test

--- mtk-automation/src/contract_actions/mod.rs
+++ mtk-automation/src/contract_actions/mod.rs
@@ -3,10 +3,10 @@ pub mod admin;
 pub mod datakey;
 pub mod fund;
 pub mod member;
+pub mod offset;
 pub mod organization;
-pub mod reward;
 pub mod token_contract;
 pub mod token_operation;
 pub mod token {
-    soroban_sdk::contractimport!(file = "./soroban_token_spec.wasm");
+    soroban_sdk::contractimport!(file = "./token_contract.wasm");
 }
