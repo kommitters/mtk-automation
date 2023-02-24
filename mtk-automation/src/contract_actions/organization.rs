a55b18d48f2b2d4a9589c1a369ac35f73234d19e
David Roldán <62166813+Odraxs@users.noreply.github.com>
2023-02-24 08:58:32 -0500

    Apply rust good practices  (#23)
    * Change functions to work with borrow
    
    * Add simplified import
    
    * Add change to functions access

--- mtk-automation/src/contract_actions/organization.rs
+++ mtk-automation/src/contract_actions/organization.rs
@@ -4,10 +4,10 @@
 use crate::contract_actions::datakey::DataKey;
 use soroban_sdk::{Env, Symbol};
 
-pub fn set_organization_name(env: &Env, new_value: Symbol) {
-    env.storage().set(&DataKey::OrgName, &new_value);
+pub(crate) fn set_organization_name(env: &Env, new_value: &Symbol) {
+    env.storage().set(&DataKey::OrgName, new_value);
 }
 
-pub fn get_organization_name(env: &Env) -> Symbol {
+pub(crate) fn get_organization_name(env: &Env) -> Symbol {
     env.storage().get(&DataKey::OrgName).unwrap().unwrap()
 }
